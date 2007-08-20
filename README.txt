06a56614cbaf4ca97557bf174eb6573ca78288c1
cmlenz <cmlenz@59ecc08e-a131-0410-9ea7-d4c0f28ac310>
2007-08-20 10:06:57 +0000

    Add Django integration code contributed by Ramiro Morales and Massimo Scamarcia.
    git-svn-id: http://svn.edgewall.org/repos/babel/contrib/django@292 59ecc08e-a131-0410-9ea7-d4c0f28ac310

---
+++ README.txt
@@ -0,0 +1,12 @@
+Tools for using Babel with Django
+=================================
+
+This package contains various utilities for integration of Babel into the
+Django web framework:
+
+ * A message extraction plugin for Django templates.
+ * A middleware class that adds the Babel `Locale` object to requests.
+
+For more information please visit the wiki page for this package:
+
+  <http://babel.edgewall.org/wiki/BabelDjango>
---
+++ babeldjango/middleware.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007 Edgewall Software
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://babel.edgewall.org/wiki/License.
+#
+# This software consists of voluntary contributions made by many
+# individuals. For the exact contribution history, see the revision
+# history and logs, available at http://babel.edgewall.org/log/.
+
+from babel import Locale, UnknownLocaleError
+try:
+    from threading import local
+except ImportError:
+    from django.utils._threading_local import local
+
+__all__ = ['get_current_locale', 'LocaleMiddleware']
+
+_thread_locals = local()
+
+def get_current_locale():
+    """Get current locale data outside views.
+
+    See http://babel.edgewall.org/wiki/ApiDocs/babel.core for Locale
+    objects documentation
+    """
+    return getattr(_thread_locals, 'locale', None)
+
+
+class LocaleMiddleware(object):
+    """Simple Django middleware that makes available a Babel `Locale` object
+    via the `request.locale` attribute.
+    """
+
+    def process_request(self, request):
+        try:
+            locale = Locale.parse(request.LANGUAGE_CODE, sep='-')
+        except (ValueError, UnknownLocaleError):
+            pass
+        else:
+            _thread_locals.locale = request.locale = locale
